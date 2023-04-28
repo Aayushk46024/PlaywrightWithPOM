4e4c24a971a5afeebc27f44face8afbde5c800a3
Aayush Khandelwal <aayush.khandelwal@infostride.com>
2023-04-28 16:56:47 +0530

    Added Extent report implementations

--- src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -3,8 +3,8 @@ package com.qa.opencart.factory;
 import com.microsoft.playwright.*;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Properties;
 
 public class PlaywrightFactory {
@@ -87,5 +87,15 @@ public class PlaywrightFactory {
             throw new RuntimeException(e);
         }
         return prop;
+    }
+
+    public static String takeScreenshot() {
+        String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+        //getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+
+        byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+        String base64Path = Base64.getEncoder().encodeToString(buffer);
+
+        return base64Path;
     }
 }
